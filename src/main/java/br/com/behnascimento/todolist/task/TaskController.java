e8ff9a33b3fa6c949f03ff6e797206e22b5454cd
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-08-09 20:57:25 -0300

    Validando usuário dono

--- src/main/java/br/com/behnascimento/todolist/task/TaskController.java
+++ src/main/java/br/com/behnascimento/todolist/task/TaskController.java
@@ -56,13 +56,24 @@ public class TaskController {
 
     // http:localhost:8080/tasks/80328082380-kcjkcjdwa-7217921789
     @PutMapping("/{id}")
-    public TaskModel updade(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    public ResponseEntity updade(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if(task == null) {
+              return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("Tarefa não encontrada");
+        }
+
         var idUser = request.getAttribute("idUser");
 
-        var task = this.taskRepository.findById(id).orElse(null);
+        if(!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Usuário não tem permissão para alterar essa tarefa");
+        }
 
         Utils.copyNonNullProperties(taskModel, task);
 
-        return this.taskRepository.save(task);
+        var taskUpdated =  this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
